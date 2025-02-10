fc0ad66a51a1835a34ffa8cb0a5e31c616c6f379
LAB14_USER ON WS15 <L14X18W15@citulab.net>
2025-02-10 19:11:54 +0800

    Reinitializing of SpringBoot due to forgetting Dependencies

---
+++ backend/mmr/mmr/src/test/java/edu/cit/mmr/MmrApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.mmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
