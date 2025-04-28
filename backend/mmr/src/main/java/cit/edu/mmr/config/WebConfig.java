8a61da60916a8183d7302b1e4e66c3503a28a7cf
Rae Duqs <raeaddison.duque@cit.edu>
2025-04-28 16:12:48 +0800

    Unblocking frontend Web

--- backend/mmr/src/main/java/cit/edu/mmr/config/WebConfig.java
+++ backend/mmr/src/main/java/cit/edu/mmr/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Replace with your frontend URL
+                .allowedOrigins("https://it-342-memoire.vercel.app","*") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Enable credentials support
