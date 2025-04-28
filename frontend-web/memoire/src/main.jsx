e8233d5f1a504c850cede8008a9fd8bb0a3980d5
Jeannie Margarett Verar <jeanniemargarett.verar@cit.edu>
2025-04-28 14:40:02 +0800

    another fix

--- frontend-web/memoire/src/main.jsx
+++ frontend-web/memoire/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Buffer } from 'buffer';
-import { BrowserRouter } from 'react-router-dom';
 
 // Polyfill for Buffer (if needed for libraries like Firebase)
 window.Buffer = Buffer;
@@ -11,8 +10,8 @@ window.global = window;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+   
       <App />
-    </BrowserRouter>
+   
   </StrictMode>
 );
