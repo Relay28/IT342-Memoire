185e2a57bb22080a1333fe5422298354b1b21572
Jeannie Margarett Verar <jeanniemargarett.verar@cit.edu>
2025-04-28 11:47:46 +0800

    fix 2.0

--- frontend-web/memoire/vite.config.js
+++ frontend-web/memoire/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "/IT342-Memoire",
+  base: process.env.VITE_BASE_PATH || "/",
   server: {
     headers: {
       'Service-Worker-Allowed': '/'
